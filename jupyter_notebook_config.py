852161a3a37240c4156ea1db03a0d07821ca9798
Tyler Kurtz <tylerkurtz8@gmail.com>
2019-01-13 14:41:34 -0500

    Working jupyter config

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,7 +1,7 @@
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
-c.MultiKernelManager.default_kernel_name = 'python2'
+c.MultiKernelManager.default_kernel_name = 'python3'
 c.NotebookApp.password = '' 
 c.NotebookApp.token = ''
 c.NotebookApp.allow_root = True
